51141fcc8b56da56a669f353bc27b34bacddb3ad
Brendan Clement <b@brendanclement.com>
2021-11-16 21:13:01 -0800

    Get wit.ai demo to work

--- Test VR/Valem/Assets/NPCSpeak.cs
+++ Test VR/Valem/Assets/NPCSpeak.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Facebook.WitAi;
+using Facebook.WitAi.Lib;
 
 public class NPCSpeak : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip audioClip;
+    public AudioClip audioClip2;
+    public AudioClip errorAudio;
 
     // Start is called before the first frame update
-    public void Speak()
+    public void Speak(WitResponseNode commandResult)
     {
-        audioSource.PlayOneShot(audioClip);
+        string intent = commandResult.GetIntentName();
+        Debug.LogError("GAME: Got intent " + commandResult.ToString());
+        if (intent == "get_name")
+        {
+            audioSource.PlayOneShot(audioClip2);
+        } 
+        else
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    public void OnError(string somethiing, string somethingElse)
+    {
+        Debug.LogError("GAME: Got Error processing wit.ai " + somethingElse + somethiing);
+        audioSource.PlayOneShot(errorAudio);
     }
 }
