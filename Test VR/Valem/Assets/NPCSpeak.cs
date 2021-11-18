1c37b1ded551b6acedb23980bfd86249a24355da
Brendan Clement <b@brendanclement.com>
2021-11-17 20:40:09 -0800

    Revert "Add all audio clips to george, refactor the speak script"
    This reverts commit 54b44218f84db49ebe9c138d0e728369c89c9407.

--- Test VR/Valem/Assets/NPCSpeak.cs
+++ Test VR/Valem/Assets/NPCSpeak.cs
@@ -7,69 +7,28 @@ using Facebook.WitAi.Lib;
 public class NPCSpeak : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip Name;
-    public AudioClip Relationship;
-    public AudioClip Occupation;
-    public AudioClip RelationshipStatus;
-    public AudioClip LastContactTime;
-    public AudioClip SuspectedKiller;
-    public AudioClip LoveLetter;
-    public AudioClip Poison;
-    public AudioClip Will;
-    public AudioClip Necklace;
-    public AudioClip[] Unknown;
+    public AudioClip audioClip;
+    public AudioClip audioClip2;
+    public AudioClip errorAudio;
 
     // Start is called before the first frame update
     public void Speak(WitResponseNode commandResult)
     {
         string intent = commandResult.GetIntentName();
         Debug.LogError("GAME: Got intent " + commandResult.ToString());
-        switch(intent)
+        if (intent == "get_name")
         {
-            case "get_name":
-                audioSource.PlayOneShot(Name);
-                break;
-            case "get_relationship_to_victim":
-                audioSource.PlayOneShot(Relationship);
-                break;
-            case "get_occupation":
-                audioSource.PlayOneShot(Occupation);
-                break;
-            case "get_relationship_status_to_victim":
-                audioSource.PlayOneShot(RelationshipStatus);
-                break;
-            case "get_last_contact_time_with_victim":
-                audioSource.PlayOneShot(LastContactTime);
-                break;
-            case "get_suspected_killer":
-                audioSource.PlayOneShot(SuspectedKiller);
-                break;
-            case "get_love_letter":
-                audioSource.PlayOneShot(LoveLetter);
-                break;
-            case "get_poison":
-                audioSource.PlayOneShot(Poison);
-                break;
-            case "get_will":
-                audioSource.PlayOneShot(Will);
-                break;
-            case "get_necklace":
-                audioSource.PlayOneShot(Necklace);
-                break;
-            default:
-                HandleUnknownIntent();
-                break;
+            audioSource.PlayOneShot(audioClip2);
+        } 
+        else
+        {
+            audioSource.PlayOneShot(audioClip);
         }
     }
 
-    public void OnError(string err, string stack)
+    public void OnError(string somethiing, string somethingElse)
     {
-        Debug.LogError("GAME: Got Error processing wit.ai " + err + stack);
-        HandleUnknownIntent();
-    }
-
-    private void HandleUnknownIntent()
-    {
-        audioSource.PlayOneShot(Unknown[Random.Range(0, Unknown.Length)]);
+        Debug.LogError("GAME: Got Error processing wit.ai " + somethingElse + somethiing);
+        audioSource.PlayOneShot(errorAudio);
     }
 }
