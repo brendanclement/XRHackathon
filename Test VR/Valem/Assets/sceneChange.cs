7e7f0f247421b5c6d988de1e18552264c3b5f048
Jordain <46907245+Jordain@users.noreply.github.com>
2021-11-17 20:21:18 -0800

    updated scenes

---
+++ Test VR/Valem/Assets/sceneChange.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    [SerializeField] RectTransform fader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, new Vector3(0, 0, 0), 0.5f).setOnComplete(() => {
+            fader.gameObject.SetActive(false);
+        });
+ 
+    }
+
+    // Update is called once per frame
+    public void OpenCrimeScene()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setOnComplete(() => {
+            SceneManager.LoadScene("CrimeScene");
+        });
+    }
+
+}
