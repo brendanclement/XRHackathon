90d35900548997591280ada9e1dda8dbfeeffaaa
Brendan Clement <b@brendanclement.com>
2021-11-19 18:29:16 -0800

    fix wit.ai voice experience

---
+++ Test VR/Valem/Assets/ActiveSpeaker.cs
@@ -0,0 +1,22 @@
+﻿using Facebook.WitAi.Lib;
+using UnityEngine;
+using Unity​Engine.XR.Interaction.Toolkit;
+
+public class ActiveSpeaker : MonoBehaviour
+{
+    private NPCSpeak activeSpeaker;
+    public void setSpeaker(DeactivateEventArgs args)
+    {
+        activeSpeaker = args.interactable.gameObject.GetComponent<NPCSpeak>();
+    }
+
+    public void Speak(WitResponseNode commandResult)
+    {
+        activeSpeaker.Speak(commandResult);
+    }
+
+    public void ErrorSpeaking(string errorCode, string stackTrace)
+    {
+        activeSpeaker.OnError(errorCode, stackTrace);
+    }
+}
