73fe1b2b99ebd375aeb79fd112eb7b17beb47792
Jordain <46907245+Jordain@users.noreply.github.com>
2021-11-20 18:59:29 -0800

    phone

--- Test VR/Valem/Assets/PlaySound.cs
+++ Test VR/Valem/Assets/PlaySound.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlaySound : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip sound;
 
-    public void play()
+    public void play(SelectEnterEventArgs args)
     {
-        audioSource.PlayOneShot(sound);
+        XRController controller;
+
+        args.interactor.TryGetComponent<XRController>(out controller);
+        if (controller != null)
+        {
+            audioSource.PlayOneShot(sound);
+        }
+        
+    }
+
+    public void stopSound()
+    {
+        audioSource.Stop();
     }
 }
